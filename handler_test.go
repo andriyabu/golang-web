81ec41d26eb07f034fa68ca6b66b4e142b489c56
ANDRI YABU NDAPA REHING <andriyabu@gmail.com>
2022-03-09 05:46:02 +0700

    Added TestHandler, TestServeMux, and TestRequest function

---
+++ handler_test.go
@@ -0,0 +1,71 @@
+package golangweb
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	var handler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello Golang")
+	}
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestServeMux(t *testing.T) {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "heloo world")
+	})
+
+	mux.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hi")
+	})
+
+	mux.HandleFunc("/images/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Images")
+	})
+
+	mux.HandleFunc("/images/thumbnail/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Thumnail")
+	})
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestRequest(t *testing.T) {
+	var handler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, r.Method)
+		fmt.Fprint(w, r.RequestURI)
+	}
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
