02e30e8d273b541642ab51d1a9874cf8bca34d76
ANDRI YABU NDAPA REHING <andriyabu@gmail.com>
2022-03-10 05:35:24 +0700

    added MultipleQueryParameter and TestMultipleQueryParameter function

--- query_param_test.go
+++ query_param_test.go
@@ -27,3 +27,22 @@ func TestQueryParameter(t *testing.T) {
 
 	fmt.Println(string(body))
 }
+
+func MultipleQueryParameter(w http.ResponseWriter, r *http.Request) {
+	firstName := r.URL.Query().Get("first_name")
+	lastName := r.URL.Query().Get("last_name")
+
+	fmt.Fprintf(w, "Hello %s %s", firstName, lastName)
+}
+
+func TestMultipleQueryParameter(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/hello?first_name=Andri&last_name=yabu", nil)
+	recorder := httptest.NewRecorder()
+	MultipleQueryParameter(recorder, request)
+
+	// response := recorder.Result()
+
+	body, _ := io.ReadAll(recorder.Result().Body) //io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+}
